a2a6a2a6a2a2548a7d84a2cb5ae22b060cbcdbef
shliu <shliu@localhost.localdomain>
2018-07-25 09:25:50 +0800

    griddata interpolation

--- direct_reconstruct.py
+++ direct_reconstruct.py
@@ -35,13 +35,19 @@ DRIFT 122.5 40 0
 end
 '''
 
+def surface_graph(xs, ys, weights):
+    x_min, x_max = xs.min(), xs.max()
+    y_min, y_max = ys.min(), ys.max()
+    x_grid = np.linspace(x_min, x_max, 200)
+    y_grid = np.linspace(y_min, y_max, 200)
+    x_mesh, y_mesh = np.meshgrid(x_grid, y_grid)
+    weights_grid = interpolate.griddata((xs, ys), weights, (x_mesh, y_mesh))
+    #ax = plt.axes(projection='3d')
+    #ax.contour3D(x_mesh, y_mesh, weights_grid, 50, cmap='viridis')
+    plt.contour(x_mesh, y_mesh, weights_grid, 15, cmap=matplotlib.cm.jet)
+    #plt.show()
+
 def part_regen(xs, xps, weights, xgrid, xpgrid, nparts_t, halo):
-    #f = weights > 0.1
-    #plt.plot(xs[f], xps[f], '.')
-    #xs = xs[noise]
-    #xps = xps[noise]
-    #weights = weights[noise]
-    h = np.histogram(weights)
     avg_x = np.average(xs, weights=weights)
     var_x = np.average((xs - avg_x)**2,  weights=weights)
     avg_xp = np.average(xps, weights=weights)
@@ -74,7 +80,7 @@ def part_regen(xs, xps, weights, xgrid, xpgrid, nparts_t, halo):
     xp_cell = np.floor((xp_new - xp_min) / dxp).astype(np.int)
     coords = np.c_[x_cell, xp_cell]
     cell_indices, indxs = np.unique(coords, axis=0, return_inverse=True)
-    weights = np.bincount(indxs, weights)
+    weights = np.bincount(indxs, weights) / np.bincount(indxs)
     particle_density[cell_indices[:, 0], cell_indices[:, 1]] = weights
 
     rst_phase_space = np.zeros((nparts_t, 2))
@@ -105,25 +111,12 @@ def part_regen(xs, xps, weights, xgrid, xpgrid, nparts_t, halo):
     
     x_rand = np.concatenate((x_rand, xs_halo))
     xp_rand = np.concatenate((xp_rand, xps_halo))
-    plt.show()
     rst_phase_space = np.c_[x_rand, xp_rand]
         
     x_new = np.cos(rot_angle) * rst_phase_space[:, 0] - np.sin(rot_angle) * rst_phase_space[:, 1]
     xp_new = np.sin(rot_angle) * rst_phase_space[:, 0] + np.cos(rot_angle) * rst_phase_space[:, 1]
     x_new = x_new + avg_x
     xp_new = xp_new + avg_xp
-    #x_min, x_max = xs.min(), xs.max()
-    #xp_min, xp_max = xps.min(), xps.max()
-    #x_range = np.linspace(x_min, x_max, xgrid)
-    #xp_range = np.linspace(xp_min, xp_max, xpgrid)
-    #x_grid, xp_grid = np.meshgrid(x_range, xp_range)
-    #density = interpolate.griddata((xs, xps), weights, (x_grid, xp_grid))
-    #ax = plt.axes(projection='3d')
-    ##ax.plot_surface(x_grid, y_grid, grid_weights, rstride=1, cstride=1,
-    ##                cmap='viridis', edgecolor='none')
-    #ax.contour3D(x_grid, xp_grid, density, 50, cmap='viridis')
-    #plt.contourf(x_grid, y_grid, grid_weights, 15, cmap=matplotlib.cm.jet)
-    plt.show()
     return x_new, xp_new
 
 def create_uniform_distr(xWidth, yWidth, nparts):
@@ -144,7 +137,7 @@ def read_emittance(filename):
     y = y - y_center
     return x, y, weights 
 
-def worker(q1, q2, q3):
+def worker(q1, q2, q3, dx, dxp, dy, dyp):
     txt_r = txt.replace('$1', str(q1))
     txt_r = txt_r.replace('$2', str(q2))
     txt_r = txt_r.replace('$3', str(q3))
@@ -153,48 +146,61 @@ def worker(q1, q2, q3):
         f.write(txt_r)
 
     cmd_str = './TraceWin MEBT.ini dst_file1=RFQ.dst'
+    cmd_str = '%s x1=%s xp1=%s' % (cmd_str, dx * 10, dxp * 1000)
+    cmd_str = '%s y1=%s yp1=%s' % (cmd_str, dy * 10, dyp * 1000)
     os.system(cmd_str)
 
-def calc_weights(dx, dy, grid_density, x, y, rot_angle, x_min, x_max, y_min, y_max, xs, ys):
-    x_new = np.cos(rot_angle) * x + np.sin(rot_angle) * y
-    y_new = -np.sin(rot_angle) * x + np.cos(rot_angle) * y
-    mask = (x_new > x_min) & (x_new < x_max) & (y_new > y_min) & (y_new < y_max)
-    xs = xs[mask]
-    ys = ys[mask]
-    x = x_new[mask]
-    y = y_new[mask]
-    weights = np.zeros_like(x)
-    x_int = ((x - x_min) / dx).astype(int)
-    y_int = ((y - y_min) / dy).astype(int)
-    x_fract = np.fmod(x - x_min, dx)
-    y_fract = np.fmod(y - y_min, dy)
-    w_lb= (1 - x_fract/dx) * (1 - y_fract/dy) * grid_density[x_int.astype(int) + 1, y_int.astype(int) + 1]
-    w_rb= x_fract/dx * (1 - y_fract/dy) * grid_density[x_int.astype(int), y_int.astype(int) + 1]
-    w_lu= (1 - x_fract/dx) * y_fract/dy * grid_density[x_int.astype(int) + 1, y_int.astype(int)]
-    w_ru=  x_fract/dx * y_fract/dy * grid_density[x_int.astype(int), y_int.astype(int)]
-    w = grid_density[x_int.astype(int), y_int.astype(int)]
-    weights = w_lb + w_rb + w_lu + w_ru
-    #f = w > 0.0001
-    #plt.plot(x[f], y[f], '.')
-    #plt.show()
+def calc_weights(x_distr_start, y_distr_start, x_distr_end, y_distr_end, x_measure, y_measure,
+                 weights_measure, xgrid=200, ygrid=200):
+    avg_x = np.average(x_measure, weights=weights_measure)
+    var_x = np.average((x_measure - avg_x)**2,  weights=weights_measure)
+    avg_y = np.average(y_measure, weights=weights_measure)
+    var_y = np.average((y_measure - avg_y)**2,  weights=weights_measure)
+    covar = np.average(x_measure * y_measure, weights=weights_measure)
+    rot_angle = np.arctan(2 * covar / (var_x - var_y)) / 2
 
-    xgrid = xpgrid = 120
-    x, xp = xs, ys
-    dx = (x.max() - x.min()) / xgrid
-    dxp = (xp.max() - xp.min()) / xpgrid
-    x_range = np.linspace(min(x), max(x), xgrid)
-    xp_range = np.linspace(min(xp), max(xp), xpgrid)
-    dx = x_range[1] - x_range[0]
-    dxp = xp_range[1] - xp_range[0]
-    x_grid, xp_grid = np.meshgrid(x_range, xp_range)
-    density = interpolate.griddata((x, xp), weights, (x_grid, xp_grid))
-    #ax = plt.axes(projection='3d')
-    #ax.plot_surface(x_grid, y_grid, grid_weights, rstride=1, cstride=1,
-    #                cmap='viridis', edgecolor='none')
-    #ax.contour3D(x_grid, xp_grid, density, 50, cmap='viridis')
-    #plt.contourf(x_grid, y_grid, grid_weights, 15, cmap=matplotlib.cm.jet)
-    #plt.show()
-    return xs, ys, weights
+    x_old = x_measure - avg_x
+    y_old = y_measure - avg_y
+    x_new = np.cos(rot_angle) * x_old + np.sin(rot_angle) * y_old
+    y_new = -np.sin(rot_angle) * x_old + np.cos(rot_angle) * y_old
+    #x_new, y_new = x_old, y_old
+
+    x_min = min(x_new)
+    x_max = max(x_new)
+    y_min = min(y_new)
+    y_max = max(y_new)
+    dx = (x_max - x_min) / xgrid
+    dy = (y_max - y_min) / ygrid
+    x_min = x_min - dx / 2
+    x_max = x_max + dx / 2
+    y_min = y_min - dy / 2
+    y_max = y_max + dy / 2
+    xgrid += 2
+    ygrid += 2
+
+    x_range = np.linspace(x_min, x_max, xgrid)
+    y_range = np.linspace(y_min, y_max, ygrid)
+    #dx = x_range[1] - x_range[0]
+    #dxp = y_range[1] - y_range[0]
+    x_grid, y_grid = np.meshgrid(x_range, y_range)
+    weights_grid = interpolate.griddata((x_new, y_new), weights_measure, (x_grid, y_grid), fill_value=0,
+                                        method='linear')
+
+    x_end_new = np.cos(rot_angle) * x_distr_end + np.sin(rot_angle) * y_distr_end
+    y_end_new = -np.sin(rot_angle) * x_distr_end + np.cos(rot_angle) * y_distr_end
+    #x_end_new, y_end_new = x_distr_end, y_distr_end
+
+    mask = (x_end_new > x_min) & (x_end_new < x_max) & (y_end_new > y_min) & (y_end_new < y_max)
+    x_end_mask = x_end_new[mask]
+    y_end_mask = y_end_new[mask]
+    x_start_mask = x_distr_start[mask]
+    y_start_mask = y_distr_start[mask]
+    #weights = np.zeros_like(x_end_mask)
+    weights = interpolate.griddata((x_grid.ravel(), y_grid.ravel()), weights_grid.ravel(),
+                                   (x_end_mask, y_end_mask), fill_value=0, method='linear')
+    #surface_graph(x_end_mask, y_end_mask, weights)
+
+    return x_start_mask, y_start_mask, weights
 
 def cast_measure_density_to_grid(xs, xps, weights, xgrid, xpgrid):
     avg_x = np.average(xs, weights=weights)
@@ -253,7 +259,7 @@ def cast_measure_density_to_grid(xs, xps, weights, xgrid, xpgrid):
 def main(hor_fname, ver_fname, I, q1, q2, q3, xgrid, xpgrid):
     xWidth = 3
     xpWidth = 5e-2
-    nparts = 100000 
+    nparts = 800000
 
     partran_dist = readDis('RFQ_base.dst') 
     xs, xps = create_uniform_distr(xWidth, xpWidth, nparts)
@@ -264,76 +270,67 @@ def main(hor_fname, ver_fname, I, q1, q2, q3, xgrid, xpgrid):
     partran_dist[:, 3] = yps
 
     generate_new_dis(partran_dist, I)
-    worker(q1, q2, q3)
 
-    xgrid = 200
-    xpgrid = 200
-    x, xp, w_x = read_emittance(hor_fname)
-    #dx = (x.max() - x.min()) / xgrid
-    #dxp = (xp.max() - xp.min()) / xpgrid
-    #x_range = np.linspace(min(x), max(x), xgrid)
-    #xp_range = np.linspace(min(xp), max(xp), xpgrid)
-    #dx = x_range[1] - x_range[0]
-    #dxp = xp_range[1] - xp_range[0]
-    #x_grid, xp_grid = np.meshgrid(x_range, xp_range)
-    density_interp_x, rot_angle_x, x_min, x_max, xp_min, xp_max, dx, dxp  =\
-        cast_measure_density_to_grid(x, xp, w_x, xgrid, xpgrid)
-    #plt.figure()
-    #plt.contourf(x_plot, xp_plot, density_interp_x, 15, cmap=matplotlib.cm.jet)
-    y, yp, w_y = read_emittance(ver_fname)
-    ygrid, ypgrid = 200, 200
-    #dy = (y.max() - y.min()) / ygrid
-    #dyp = (yp.max() - yp.min()) / ypgrid
-    #y_range = np.linspace(min(y), max(y), ygrid)
-    #yp_range = np.linspace(min(yp), max(yp), ypgrid)
-    #dy = y_range[1] - y_range[0]
-    #dyp = yp_range[1] - yp_range[0]
-    #y_grid, yp_grid = np.meshgrid(y_range, yp_range)
-    density_interp_y, rot_angle_y, y_min, y_max, yp_min, yp_max, dy, dyp =\
-        cast_measure_density_to_grid(y, yp, w_y, ygrid, ypgrid)
+    for i in range(6):
+        dx, dxp = np.average(xs), np.average(xps)
+        dy, dyp = np.average(ys), np.average(yps)
+        worker(q1, q2, q3, dx, dxp, dy, dyp)
 
-    distr_slit = exitDis('results/dtl1.plt')
+        xgrid = 200
+        xpgrid = 200
+        x, xp, w_x = read_emittance(hor_fname)
+        dx = (x.max() - x.min()) / xgrid
+        dxp = (xp.max() - xp.min()) / xpgrid
+        x_range = np.linspace(min(x), max(x), xgrid)
+        xp_range = np.linspace(min(xp), max(xp), xpgrid)
+        #dx = x_range[1] - x_range[0]
+        #dxp = xp_range[1] - xp_range[0]
+        x_grid, xp_grid = np.meshgrid(x_range, xp_range)
+        weights_grid_x = interpolate.griddata((x, xp), w_x, (x_grid, xp_grid), fill_value=0)
+        #plt.figure()
+        #plt.contourf(x_plot, xp_plot, density_interp_x, 15, cmap=matplotlib.cm.jet)
+        y, yp, w_y = read_emittance(ver_fname)
+        ygrid, ypgrid = 200, 200
+        dy = (y.max() - y.min()) / ygrid
+        dyp = (yp.max() - yp.min()) / ypgrid
+        y_range = np.linspace(min(y), max(y), ygrid)
+        yp_range = np.linspace(min(yp), max(yp), ypgrid)
+        #dy = y_range[1] - y_range[0]
+        #dyp = yp_range[1] - yp_range[0]
+        y_grid, yp_grid = np.meshgrid(y_range, yp_range)
+        weights_grid_y = interpolate.griddata((y, yp), w_y, (y_grid, yp_grid), fill_value=0)
 
-    #f_x = Rbf(x, xp, w_x)
-    #plt.figure()
-    #density= f_x(x_plot.ravel(), xp_plot.ravel())
-    #plt.contourf(x_plot, xp_plot, density.reshape(200, 200), 15, cmap=matplotlib.cm.jet)
-    #plt.show()
+        distr_slit = exitDis('results/dtl1.plt')
 
-    #x_filter = ((distr_slit[:, 0] * 10 < x.max()) & (distr_slit[:, 0] * 10 > x.min()))
-    #xp_filter = ((distr_slit[:, 1] * 1000 < xp.max()) & (distr_slit[:, 1] * 1000 > xp.min()))
-    #x_plane_filter = (x_filter & xp_filter)
+        xs, xps, weights_x = calc_weights(xs, xps, distr_slit[:, 0] * 10, distr_slit[:, 1] * 1000,
+                                          x, xp, w_x)
 
-    #f_y = Rbf(y, yp, w_y)
-    #y_filter = ((distr_slit[:, 2] * 10 < y.max()) & (distr_slit[:, 2] * 10 > y.min()))
-    #yp_filter = ((distr_slit[:, 3] * 1000 < yp.max()) & (distr_slit[:, 3] * 1000 > yp.min()))
-    #y_plane_filter = y_filter & yp_filter
+        #weights_x = f_x(distr_new[:, 0] * 10, distr_new[:, 1] * 1000)
+        #weights_y = f_y(distr_new[:, 2] * 10, distr_new[:, 3] * 1000)
+        ys, yps, weights_y = calc_weights(ys, yps, distr_slit[:, 2] * 10, distr_slit[:, 3] * 1000,
+                                          y, yp, w_y)
+        #surface_graph(ys, yps, weights_y)
+        #plt.plot(ys, yps, 'r.')
+        #f = weights_y > weights_y.max() * 0.005
 
-    #distr_filter = x_plane_filter & y_plane_filter
-    #distr_new = distr_slit[distr_filter]
+        #plt.plot(ys[f], yps[f], 'b.')
+        #xs, xps = xs[distr_filter], xps[distr_filter]
+        #ys, yps = ys[distr_filter], yps[distr_filter]
 
-    #x_slit, xp_slit, y_slit, yp_slit = distr_slit[:, 0], distr_slit[:, 1], distr_slit[:, 2], distr_slit[:, 3]
-    #x = np.cos(rot_angle) * x + np.sin(rot_angle) * y
-    #y = -np.sin(rot_angle) * x + np.cos(rot_angle) * y
-    #mask = (x > x_min) & (x < x_max) & (y > y_min) & (y < y_max)
-    xs, xps, weights_x = calc_weights(dx, dxp, density_interp_x, distr_slit[:, 0] * 10, distr_slit[:, 1] * 1000, rot_angle_x,
-                             x_min, x_max, xp_min, xp_max, xs, xps)
-    #weights_x = f_x(distr_new[:, 0] * 10, distr_new[:, 1] * 1000)
-    #weights_y = f_y(distr_new[:, 2] * 10, distr_new[:, 3] * 1000)
-    ys, yps, weights_y = calc_weights(dy, dyp, density_interp_y, distr_slit[:, 2] * 10, distr_slit[:, 3] * 1000, rot_angle_y,
-                             y_min, y_max, yp_min, yp_max, ys, yps)
-    #xs, xps = xs[distr_filter], xps[distr_filter]
-    #ys, yps = ys[distr_filter], yps[distr_filter]
+        #plt.plot(ys, yps, 'r.')
+        xgrid = xpgrid = 120
+        xs, xps = part_regen(xs, xps, weights_x, xgrid, xpgrid, nparts, halo=False)
+        ys, yps = part_regen(ys, yps, weights_y, xgrid, xpgrid, nparts, halo=False)
+        #plt.plot(ys, yps, 'b.')
+        #plt.show()
 
-    xs, xps = part_regen(xs, xps, weights_x, xgrid, xpgrid, nparts, halo=False)
-    ys, yps = part_regen(ys, yps, weights_y, xgrid, xpgrid, nparts, halo=False)
 
-    partran_dist[:, 0] = xs
-    partran_dist[:, 1] = xps
-    partran_dist[:, 2] = ys
-    partran_dist[:, 3] = yps
+        partran_dist[:, 0] = xs
+        partran_dist[:, 1] = xps
+        partran_dist[:, 2] = ys
+        partran_dist[:, 3] = yps
 
-    generate_new_dis(partran_dist, I)
+        generate_new_dis(partran_dist, I)
 
 if __name__ == '__main__':
     h_fname = 'hor-5mA'
